43263df591035aab6858364f6e5ba8eab9ac6c0c
Angel <angelghstm@gmail.com>
2021-03-18 22:56:56 +0100

    ejercicio1 php

---
+++ ejer.php
@@ -0,0 +1,16 @@
+<?php
+
+
+abstract class Unit {
+    protected $alive = true;
+    protected $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+
+
+
+?>
---
+++ ejercicio1.php
@@ -0,0 +1,35 @@
+<?php
+echo "<p> hola mundo </p>" ;
+$int = 5 ;
+echo  " <p> $int </p> ";
+$double = 5.4 ;
+echo  " <p> $double </p> ";
+$string = 'hola' ;
+echo  " <p> $string </p> ";
+$boolean = 0 ;
+echo  " <p> $boolean </p> ";
+  if($boolean) {
+    echo "verdadero" ;
+      } 
+   else {
+  echo "falso";
+   }
+
+//1. Longitud de uns string
+
+$cadena1 = "angel";
+echo "<p> La longitud de cadena que dice $cadena1 es " . strlen($cadena1) . "</p>";
+
+//2. invertir string
+
+echo strrev($cadena1)."<br>";
+$cadena2 ="garciadiego";
+
+
+//3. concatenar 2 string  
+
+echo "$cadena1 $cadena2" ;
+
+
+
+?>
